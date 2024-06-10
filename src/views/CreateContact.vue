0870e617dd3bd387e6f246250e04428f5c0865ba
ladthomas <ladouyouthomas@outlook.fr>
2024-06-10 16:53:38 +0200

    modified create contact

--- src/views/CreateContact.vue
+++ src/views/CreateContact.vue
@@ -2,6 +2,9 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/"></ion-back-button>
+        </ion-buttons>
         <ion-title>Create Contact</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -11,19 +14,19 @@
           <ion-input label="Name" v-model="contact.name"></ion-input>
         </ion-item>
         <ion-item>
-          <ion-input label="Email" v-model="contact.email" placeholder="Enter email"></ion-input>
+          <ion-input label="Email" v-model="contact.email" placeholder="Saisir l'adresse e-mail"></ion-input>
         </ion-item>
         <ion-item>
-          <ion-input label="Phone" v-model="contact.phone" placeholder="Enter phone number"></ion-input>
+          <ion-input label="Phone" v-model="contact.phone" placeholder="Entrez le numéro de téléphone"></ion-input>
         </ion-item>
-        <ion-button expand="full" @click="createContact">Create</ion-button>
+        <ion-button expand="full" @click="createContact">Créer nouveau contact</ion-button>
       </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonButton , IonBackButton } from '@ionic/vue';
 
 export default {
   components: {
@@ -35,7 +38,8 @@ export default {
     IonList,
     IonItem,
     IonInput,
-    IonButton
+    IonButton,
+    IonBackButton
   },
   data() {
     return {
